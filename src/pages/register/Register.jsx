0c8e82541e6a9cd5327dfe4e4aaae2853d5b015e
Mustafa Karakaş <mustafakarakas09@gmail.com>
2022-08-02 21:15:28 +0300

    first commit

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import underConst from '../../assets/const.png';
+
+const Register = () => {
+    return (
+        <div>
+            <img style={{ width: '90%', margin: '4rem 0' }} src={underConst} alt="" />
+        </div>
+    );
+};
+
+export default Register;
+
